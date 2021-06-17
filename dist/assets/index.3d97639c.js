aec1736062bbd74578b61125a4521794e7abee18
MX48@Live <MX48Live@Gmail.com>
2021-06-17 23:23:04 +0700

    Add Firebase

---
+++ dist/assets/index.3d97639c.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$("header ul > li > a").parent().has("ul").on("click",(function(){$(this).toggleClass("showSubMenu")})),$("header .logo .toggle-mobile").on("click",(function(){$("header nav").toggleClass("show"),$("header ul > li").removeClass("showSubMenu")}))}));
