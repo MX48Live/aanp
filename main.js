94d96492d13a19e4dc6728ca30d17737aed8d211
MX48@Live <MX48Live@Gmail.com>
2021-06-19 22:33:16 +0700

    Add slick to all

--- main.js
+++ main.js
@@ -9,48 +9,9 @@ $(document).ready(function () {
     $('header ul > li').removeClass('showSubMenu')
   })
 
-  $('#slick-insight').slick({
+  $('#slick-hero .slick-app').slick({
     nextArrow: '<button type="button" class="slick-next"><span class="material-icons">chevron_right</span></button>',
-    prevArrow: '<button type="button" class="slick-next"><span class="material-icons">chevron_left</span></button>',
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: false,
-    responsive: [
-      {
-        breakpoint: 1213,
-        settings: {
-          slidesToShow: 2,
-          centerMode: false,
-          centerPadding: '10%',
-          dots: false,
-        }
-      },
-      {
-        breakpoint: 1007,
-        settings: {
-          arrows: false,
-          slidesToShow: 2,
-          centerMode: true,
-          centerPadding: '10%',
-          dots: true,
-        }
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          arrows: false,
-          slidesToShow: 1,
-          centerMode: true,
-          centerPadding: '10%',
-          dots: true,
-        }
-      },
-    ]
-  });
-
-  $('#slick-clients .slick-app').slick({
-    nextArrow: '<button type="button" class="slick-next"><span class="material-icons">chevron_right</span></button>',
-    prevArrow: '<button type="button" class="slick-next"><span class="material-icons">chevron_left</span></button>',
+    prevArrow: '<button type="button" class="slick-prev"><span class="material-icons">chevron_left</span></button>',
     appendDots: $('#slick-clients'),
     dots: true,
     responsive: [
@@ -62,7 +23,53 @@ $(document).ready(function () {
       },
     ]
   });
-  $('#slick-we-aap').slick({
+
+  $('#slick-insight').slick({
+    nextArrow: '<button type="button" class="slick-next"><span class="material-icons">chevron_right</span></button>',
+    prevArrow: '<button type="button" class="slick-prev"><span class="material-icons">chevron_left</span></button>',
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1213,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerMode: false,
+          centerPadding: '10%',
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 1007,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerMode: true,
+          centerPadding: '10%',
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: '10%',
+          dots: true,
+        }
+      },
+    ]
+  });
+
+  $('#slick-clients .slick-app').slick({
+    nextArrow: '<button type="button" class="slick-next"><span class="material-icons">chevron_right</span></button>',
+    prevArrow: '<button type="button" class="slick-prev"><span class="material-icons">chevron_left</span></button>',
+    appendDots: $('#slick-clients'),
     dots: true,
     responsive: [
       {
@@ -73,4 +80,60 @@ $(document).ready(function () {
       },
     ]
   });
+
+  $('#slick-we-aap .slick-app').slick({
+    nextArrow: '<button type="button" class="slick-next"><span class="material-icons">chevron_right</span></button>',
+    prevArrow: '<button type="button" class="slick-prev"><span class="material-icons">chevron_left</span></button>',
+    appendDots: $('#slick-we-aap'),
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1007,
+        settings: {
+          arrows: false,
+        }
+      },
+    ]
+  });
+
+  $('#slick-what-we-offer .slick-app').slick({
+    nextArrow: '<button type="button" class="slick-next"><span class="material-icons">chevron_right</span></button>',
+    prevArrow: '<button type="button" class="slick-prev"><span class="material-icons">chevron_left</span></button>',
+    appendDots: $('#slick-what-we-offer'),
+    dots: true,
+    arrows: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1415,
+        settings: {
+          arrows: true,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 1213,
+        settings: {
+          arrows: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 1007,
+        settings: {
+          arrows: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: "unslick"
+      },
+    ]
+  });
+
 })
